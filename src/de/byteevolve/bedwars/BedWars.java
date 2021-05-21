310b355a4fc745acfd03340335ab73eb27a99181
RoyalByte <adrian.schiel.2904@gmail.com>
2021-05-21 18:01:03 +0200

    Addet laodVersions etc.

--- src/de/byteevolve/bedwars/BedWars.java
+++ src/de/byteevolve/bedwars/BedWars.java
@@ -3,8 +3,15 @@ package de.byteevolve.bedwars;
 
 import de.byteevolve.bedwars.arena.Arena;
 import de.byteevolve.bedwars.arena.ArenaHandler;
+import de.byteevolve.bedwars.commands.Command_Arena;
+import de.byteevolve.bedwars.configuration.ConfigHandler;
+import de.byteevolve.bedwars.configuration.config.ConfigEntries;
 import de.byteevolve.bedwars.database.MySQL;
+import de.byteevolve.bedwars.itembuilder.unbreakable.*;
 import de.byteevolve.bedwars.location.LocationHandler;
+import de.byteevolve.bedwars.player.actionbar.*;
+import de.byteevolve.bedwars.player.respawn.*;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
 
@@ -15,26 +22,112 @@ public class BedWars extends JavaPlugin {
     private LocationHandler locationHandler;
     private ArenaHandler arenaHandler;
     public static int players, teams;
+    private BWActionbar actionbar;
+    private BWRespawn respawn;
+    private Unbreakable unbreakable;
+    private ConfigHandler configHandler;
+    private String prefix,noPerm, mustAPlayer,playerNotOnline;
 
     @Override
     public void onEnable() {
         instance = this;
+        this.configHandler = new ConfigHandler();
+        this.prefix = ConfigEntries.PREFIX.getAsString();
+        this.noPerm = this.prefix + ConfigEntries.NOPERM.getAsString();
+        this.mustAPlayer = this.prefix + ConfigEntries.MUSTAPLAYER.getAsString();
+        this.playerNotOnline = this.prefix + ConfigEntries.PLAYERNOTONLINE.getAsString();
+
         this.mySQL = new MySQL("localhost", "root", "", "bedwars", 3306);
         this.locationHandler = new LocationHandler();
         this.arenaHandler = new ArenaHandler();
 
-        Arena arena = new Arena("Test");
-        arena.update();
-        new BukkitRunnable(){
+        getCommand("arena").setExecutor(new Command_Arena());
 
-            @Override
-            public void run() {
-                arena.setPlayers(10);
-                arena.setDisplayname("&aLOL");
-                arena.update();
+    }
+
+    private void loadVersions(){
+        String version = "N/A";
+        try{
+            version = Bukkit.getServer().getClass().getPackage().getName().split("\\.")[3];
+
+            switch (version) {
+                case "v1_8_R3":
+                    this.actionbar = new v1_8_R3_Actionbar();
+                    this.respawn = new v1_8_R3_Respawn();
+                    this.unbreakable = new v1_8_R3_Unbreakable();
+                    break;
+                case "v1_9_R2":
+                    this.actionbar = new v1_9_R2_Actionbar();
+                    this.respawn = new v1_9_R2_Respawn();
+                    this.unbreakable = new v1_9_R2_Unbreakable();
+                    break;
+                case "v1_10_R1":
+                    this.actionbar = new v1_10_R1_Actionbar();
+                    this.respawn = new v1_10_R1_Respawn();
+                    this.unbreakable = new v1_10_R1_Unbreakable();
+                    break;
+                case "v1_11_R1":
+                    this.actionbar = new v1_11_R1_Actionbar();
+                    this.respawn = new v1_11_R1_Respawn();
+                    this.unbreakable = new v1_11_R1_Unbreakable();
+                    break;
+                case "v1_12_R1":
+                    this.actionbar = new v1_12_R1_Actionbar();
+                    this.respawn = new v1_12_R1_Respawn();
+                    this.unbreakable = new v1_12_R1_Unbreakable();
+                    break;
+                case "v1_13_R2":
+                    this.actionbar = new v1_13_R2_Actionbar();
+                    this.respawn = new v1_13_R2_Respawn();
+                    this.unbreakable = new v1_13_R2_Unbreakable();
+                    break;
+                case "v1_14_R1":
+                    this.actionbar = new v1_14_R1_Actionbar();
+                    this.respawn = new v1_14_R1_Respawn();
+                    this.unbreakable = new v1_14_R1_Unbreakable();
+                    break;
+                case "v1_15_R1":
+                    this.actionbar = new v1_15_R1_Actionbar();
+                    this.respawn = new v1_15_R1_Respawn();
+                    this.unbreakable = new v1_15_R1_Unbreakable();
+                    break;
+                case "v1_16_R3_Respawn":
+                    this.actionbar = new v1_16_R3_Actionbar();
+                    this.respawn = new v1_16_R3_Respawn();
+                    this.unbreakable = new v1_16_R3_Unbreakable();
+                    break;
             }
-        }.runTaskLaterAsynchronously(this, 100);
+        }catch (ArrayIndexOutOfBoundsException ex){
+            ex.printStackTrace();
+        }
+    }
 
+    public String getPrefix() {
+        return prefix;
+    }
+
+    public String getNoPerm() {
+        return noPerm;
+    }
+
+    public String getMustAPlayer() {
+        return mustAPlayer;
+    }
+
+    public String getPlayerNotOnline() {
+        return playerNotOnline;
+    }
+
+    public BWActionbar getActionbar() {
+        return actionbar;
+    }
+
+    public BWRespawn getRespawn() {
+        return respawn;
+    }
+
+    public Unbreakable getUnbreakable() {
+        return unbreakable;
     }
 
     public LocationHandler getLocationHandler() {
