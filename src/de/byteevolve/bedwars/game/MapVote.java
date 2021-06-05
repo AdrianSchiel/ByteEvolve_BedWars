b87be33387033bde2fc52da80230716a88e5cf9f
RoyalByte <adrian.schiel.2904@gmail.com>
2021-06-05 15:37:51 +0200

    addet PlayerVotes

--- src/de/byteevolve/bedwars/game/MapVote.java
+++ src/de/byteevolve/bedwars/game/MapVote.java
@@ -1,6 +1,8 @@
 package de.byteevolve.bedwars.game;
 
 import de.byteevolve.bedwars.arena.Arena;
+import de.byteevolve.bedwars.player.PlayerHandler;
+import org.bukkit.entity.Player;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -8,9 +10,21 @@ import java.util.Map;
 public class MapVote {
 
     private Map<Arena, Integer> votes;
+    private Map<Player, Arena> playerVotes;
 
     public MapVote() {
         this.votes = new HashMap<>();
+        this.playerVotes = new HashMap<>();
+    }
+
+    public Map<Player, Arena> getPlayerVotes() {
+        return playerVotes;
+    }
+
+    public boolean hasVoted(Player player){
+        if(this.playerVotes.containsKey(player)) return true;
+
+        return false;
     }
 
     public Map<Arena, Integer> getVotes() {
