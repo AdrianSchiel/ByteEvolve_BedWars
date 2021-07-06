ff97cf24312a43b824b6762fa8aff05db7414268
RoyalByte <adrian.schiel.2904@gmail.com>
2021-07-06 20:00:50 +0200

    removeVoteItems on start

--- src/de/byteevolve/bedwars/game/GameTimer.java
+++ src/de/byteevolve/bedwars/game/GameTimer.java
@@ -1,8 +1,11 @@
 package de.byteevolve.bedwars.game;
 
 import de.byteevolve.bedwars.BedWars;
+import de.byteevolve.bedwars.configuration.config.ConfigEntries;
 import org.bukkit.Bukkit;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.scheduler.BukkitRunnable;
 
 public class GameTimer extends BukkitRunnable {
@@ -20,8 +23,10 @@ public class GameTimer extends BukkitRunnable {
 
         if(this.count == 10){
             gameHandler.loadResults();
-
             for(Player player : Bukkit.getOnlinePlayers()){
+                player.getInventory().setItem(0, new ItemStack(Material.AIR, 1));
+                player.getInventory().setItem(1, new ItemStack(Material.AIR, 1));
+                player.getInventory().setItem(4, new ItemStack(Material.AIR, 1));
                 if(gameHandler.isPlayerInTeam(player) == null){
                     for(Team team : gameHandler.getTeams()){
                         if(!(team.getMembers().size() >= ConfigEntries.PLAYERSPERTEAM.getAsInt())){
