d1a4d0b917c091fe239f757c168673d1b9064f34
ByteRoyal <ByteRoyal@gmx.de>
2021-08-23 19:31:06 +0200

    Added rejoin / spectate feature

--- src/de/byteevolve/bedwars/listener/Listener_Quit.java
+++ src/de/byteevolve/bedwars/listener/Listener_Quit.java
@@ -1,6 +1,8 @@
+
 package de.byteevolve.bedwars.listener;
 
 import de.byteevolve.bedwars.BedWars;
+import de.byteevolve.bedwars.game.Team;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -8,21 +10,27 @@ import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.scheduler.BukkitRunnable;
 
 public class Listener_Quit implements Listener {
-
+    public Listener_Quit() {
+    }
 
     @EventHandler
-    public void onQuit(PlayerQuitEvent event){
+    public void onQuit(PlayerQuitEvent event) {
         Player player = event.getPlayer();
-        switch (BedWars.getInstance().getGameHandler().getGameState()){
+        switch (BedWars.getInstance().getGameHandler().getGameState()) {
             case LOBBY:
-                new BukkitRunnable(){
-                    @Override
+                (new BukkitRunnable() {
                     public void run() {
                         BedWars.getInstance().getGameHandler().manageGameStart();
                     }
-                }.runTaskLaterAsynchronously(BedWars.getInstance(), 3);
+                }).runTaskLaterAsynchronously(BedWars.getInstance(), 3L);
                 break;
+            case INGAME:
+                if (BedWars.getInstance().getGameHandler().isPlayerInTeam(player) != null) {
+                    Team team = BedWars.getInstance().getGameHandler().isPlayerInTeam(player);
+                    String prefix = BedWars.getInstance().getPrefix();
+                    event.setQuitMessage(prefix + "§8Der Spieler " + team.getTeam().getColor() + player.getName() + " §8hat das Spiel §cverlassen");
+                }
         }
-    }
 
+    }
 }
