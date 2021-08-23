13e830462a6f702961d429fe479b55019bc660be
ByteRoyal <ByteRoyal@gmx.de>
2021-08-23 19:27:18 +0200

    organized ConfigEntry

--- src/de/byteevolve/bedwars/configuration/ConfigEntry.java
+++ src/de/byteevolve/bedwars/configuration/ConfigEntry.java
@@ -1,15 +1,18 @@
+
 package de.byteevolve.bedwars.configuration;
 
 import de.byteevolve.bedwars.configuration.config.ConfigSections;
 
 public interface ConfigEntry {
-
     String getPath();
+
     Object getValue();
+
     Object getDefaultValue();
-    void setValue(Object value);
+
+    void setValue(Object var1);
+
     ConfigSections getSection();
+
     String getDescription();
-
-
 }
