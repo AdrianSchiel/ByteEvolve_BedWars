c7553a1f6f12c9edf1d7067d1ae19a2702a6c976
ByteRoyal <ByteRoyal@gmx.de>
2021-08-31 19:16:18 +0200

    Init new shop category´s

--- src/de/byteevolve/bedwars/shop/config/ShopEntry.java
+++ src/de/byteevolve/bedwars/shop/config/ShopEntry.java
@@ -2,13 +2,33 @@ package de.byteevolve.bedwars.shop.config;
 
 import de.byteevolve.bedwars.configuration.ConfigEntry;
 import de.byteevolve.bedwars.configuration.config.ConfigSections;
+import org.bukkit.Material;
 
 public enum ShopEntry implements ConfigEntry {
+    CURRENCY1(ConfigSections.SHOP_CURRENCY, "CURRENCY_1", "HARDEND_CLAY", "HARDEND_CLAY", 0, 0, "HARDEND_CLAY", "HARDEND_CLAY"),
+    CURRENCY2(ConfigSections.SHOP_CURRENCY, "CURRENCY_2", "IRON", "IRON", 0, 0, "IRON", "IRON"),
+    CURRENCY3(ConfigSections.SHOP_CURRENCY, "CURRENCY_3", "GOLD", "GOLD", 0, 0, "GOLD", "GOLD"),
+
+    UTILITY1(ConfigSections.SHOP_UTILITY, "UTILITY_1", "SANDSTONE", "SANDSTONE", 10, 10, "GOLD", "GOLD"),
+    UTILITY2(ConfigSections.SHOP_UTILITY, "UTILITY_2", "SANDSTONE", "WOOL", 10, 10, "IRON", "IRON"),
+    UTILITY3(ConfigSections.SHOP_UTILITY, "UTILITY_3", "SANDSTONE", "STONE", 10, 10, "IRON", "IRON"),
+    UTILITY4(ConfigSections.SHOP_UTILITY, "UTILITY_4", "SANDSTONE", "WOOD", 10, 10, "HARDEND_CLAY", "HARDEND_CLAY"),
+    UTILITY5(ConfigSections.SHOP_UTILITY, "UTILITY_5", "SANDSTONE", "SANDSTONE", 10, 10, "GOLD", "GOLD"),
+
+    TOOLS1(ConfigSections.SHOP_TOOLS, "TOOLS_1", "WOOD_AXE", "WOOD_AXE", 10, 10, "GOLD", "GOLD"),
+    TOOLS2(ConfigSections.SHOP_TOOLS, "TOOLS_2", "WOOD_PICKAXE", "WOOD_PICKAXE", 10, 10, "IRON", "IRON"),
+    TOOLS3(ConfigSections.SHOP_TOOLS, "TOOLS_3", "SHEARS", "SHEARS", 10, 10, "IRON", "IRON"),
+    TOOLS4(ConfigSections.SHOP_TOOLS, "TOOLS_4", "STONE_AXE", "STONE_AXE", 10, 10, "HARDEND_CLAY", "HARDEND_CLAY"),
+    TOOLS5(ConfigSections.SHOP_TOOLS, "TOOLS_5", "STONE_PICKAXE", "STONE_PICKAXE", 10, 10, "GOLD", "GOLD"),
+    TOOLS6(ConfigSections.SHOP_TOOLS, "TOOLS_4", "IRON_AXE", "IRON_AXE", 10, 10, "HARDEND_CLAY", "HARDEND_CLAY"),
+    TOOLS7(ConfigSections.SHOP_TOOLS, "TOOLS_5", "IRON_PICKAXE", "IRON_PICKAXE", 10, 10, "GOLD", "GOLD"),
+
     BLOCK1(ConfigSections.SHOP_BLOCKS, "BLOCK_1", "SANDSTONE", "SANDSTONE", 10, 10, "GOLD", "GOLD"),
     BLOCK2(ConfigSections.SHOP_BLOCKS, "BLOCK_2", "SANDSTONE", "WOOL", 10, 10, "IRON", "IRON"),
     BLOCK3(ConfigSections.SHOP_BLOCKS, "BLOCK_3", "SANDSTONE", "STONE", 10, 10, "IRON", "IRON"),
     BLOCK4(ConfigSections.SHOP_BLOCKS, "BLOCK_4", "SANDSTONE", "WOOD", 10, 10, "HARDEND_CLAY", "HARDEND_CLAY"),
     BLOCK5(ConfigSections.SHOP_BLOCKS, "BLOCK_5", "SANDSTONE", "SANDSTONE", 10, 10, "GOLD", "GOLD");
+
 
     private String path;
     private String defvalue;
