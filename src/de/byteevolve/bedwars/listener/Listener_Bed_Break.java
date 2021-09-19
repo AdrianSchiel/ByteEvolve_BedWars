a2bce7f435ab4b3c38b41bc8e87b271bf8b48b46
ByteRoyal <ByteRoyal@gmx.de>
2021-09-19 22:27:19 +0200

    Innit bed break and team removal

--- src/de/byteevolve/bedwars/listener/Listener_Bed_Break.java
+++ src/de/byteevolve/bedwars/listener/Listener_Bed_Break.java
@@ -5,7 +5,7 @@ import de.byteevolve.bedwars.arena.Teams;
 import de.byteevolve.bedwars.game.Team;
 import de.byteevolve.bedwars.location.Loc;
 import de.byteevolve.bedwars.location.LocationHandler;
-import jdk.tools.jaotc.Main;
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -16,14 +16,20 @@ public class Listener_Bed_Break implements Listener {
     @EventHandler
     public void onBreak(BlockBreakEvent event) {
         if (event.getBlock().getType().equals(Material.BED_BLOCK)) {
-            for (String string : BedWars.getInstance().getGameHandler().getArena().getBeds()) {
-                Loc loc = BedWars.getInstance().getLocationHandler().getLocByName(string);
-                if (event.getBlock().getLocation().equals(loc.getAsLocation())) {
-                    System.out.println(Integer.parseInt(string.split("team")[0].substring(0)));
-                    int id = Integer.parseInt(string.split("team")[0].substring(0));
-                    // Teams teams = Teams.fromID(id);
+            for (Loc loc : BedWars.getInstance().getLocationHandler().getLocList("bed")) {
+                loc.setYaw(0);
+                loc.setPitch(0);
+                if (event.getBlock().getLocation().toString().contains(loc.getAsLocation().toString())) {
+                    int id = Integer.parseInt(loc.getName().split("team")[1].split("bed")[0]);
+                    Teams teams = Teams.fromID(id);
                     Team team = BedWars.getInstance().getGameHandler().getTeams().get(id);
-                    team.setBed(false);
+                    if (!team.getMembers().contains(event.getPlayer())) {
+                        Bukkit.getServer().broadcastMessage(BedWars.getInstance().getPrefix() + "§8The bed from team " + teams.getColor() + teams.name() + " §8was destroyed!");
+                        team.setBed(false);
+                    }else {
+                        event.getPlayer().sendMessage(BedWars.getInstance().getPrefix() + "§8You cant destroy your own bed!");
+                    event.setCancelled(true);
+                    }
 
                 }
 
