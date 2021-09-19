d7289e409bb584ae748e02e6b90019492ff9b897
ByteRoyal <ByteRoyal@gmx.de>
2021-09-19 22:26:21 +0200

    Innit player respawn and death event

--- src/de/byteevolve/bedwars/listener/Listener_Player_Death.java
+++ src/de/byteevolve/bedwars/listener/Listener_Player_Death.java
@@ -8,6 +8,7 @@ import org.bukkit.Location;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.event.player.PlayerRespawnEvent;
 import org.bukkit.material.Bed;
 
 public class Listener_Player_Death implements Listener {
@@ -17,10 +18,17 @@ public class Listener_Player_Death implements Listener {
             BedWars.getInstance().getGameHandler().getTeam(event.getEntity()).getMembers().remove(event.getEntity());
             event.getEntity().spigot().respawn();
             event.getEntity().setGameMode(GameMode.SPECTATOR);
-        } else
-            event.getEntity().spigot().respawn();
-        Teams teams = BedWars.getInstance().getGameHandler().getTeam(event.getEntity()).getTeam();
-        Location location = BedWars.getInstance().getLocationHandler().getLocByName(BedWars.getInstance().getGameHandler().getArena().getName() + "team" + teams.getId() + "spawn").getAsLocation();
-        event.getEntity().teleport(location);
+        }
+    }
+    @EventHandler
+    public void onRespawn(PlayerRespawnEvent event){
+        if (BedWars.getInstance().getGameHandler().getTeam(event.getPlayer()) == null) {
+            event.getPlayer().setGameMode(GameMode.SPECTATOR);
+        } else {
+            Teams teams = BedWars.getInstance().getGameHandler().getTeam(event.getPlayer()).getTeam();
+            Location location = BedWars.getInstance().getLocationHandler().getLocByName(BedWars.getInstance().getGameHandler().getArena().getName() + "team" + teams.getId() + "spawn").getAsLocation();
+            event.getPlayer().teleport(location);
+
+        }
     }
 }
