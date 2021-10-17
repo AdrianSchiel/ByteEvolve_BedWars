3ec74ad7329146b877c93e98cb3fb2659be537b0
ByteRoyal <ByteRoyal@gmx.de>
2021-10-17 19:43:34 +0200

    Added scoreboard support

--- src/de/byteevolve/bedwars/player/scoreboard/v1_9_R2_Scoreboard.java
+++ src/de/byteevolve/bedwars/player/scoreboard/v1_9_R2_Scoreboard.java
@@ -1,7 +1,11 @@
 package de.byteevolve.bedwars.player.scoreboard;
 
 import de.byteevolve.bedwars.BedWars;
+import de.byteevolve.bedwars.arena.Teams;
 import de.byteevolve.bedwars.configuration.config.ConfigEntries;
+import de.byteevolve.bedwars.game.GameHandler;
+import de.byteevolve.bedwars.game.GameState;
+import de.byteevolve.bedwars.game.Team;
 import de.byteevolve.bedwars.player.stats.PlayerStats;
 import de.byteevolve.bedwars.player.stats.PlayerStatsType;
 import net.minecraft.server.v1_9_R2.*;
@@ -19,33 +23,64 @@ public class v1_9_R2_Scoreboard implements BWScoreboard {
         obj.setDisplayName("BEDWARS");
 
         PlayerStats playerStats = new PlayerStats(player.getUniqueId().toString());
+        if (BedWars.getInstance().getGameHandler().getGameState().equals(GameState.INGAME)) {
+            PacketPlayOutScoreboardObjective removePacket = new PacketPlayOutScoreboardObjective(obj, 1);
+            sendPacket(removePacket, player);
+            sendPacket(createpacket, player);
+            sendPacket(display, player);
+            int i = 1;
+            GameHandler gameHandler = BedWars.getInstance().getGameHandler();
+            for (Team team : gameHandler.getTeams()) {
+                String bed;
+                Teams teams = team.getTeam();
+                if (team.isBed()) {
+                    bed = "§a❤";
+                } else
+                    bed = "§c❤";
+               ScoreboardScore score = new ScoreboardScore(sb, obj, teams.getColor() + teams.name() + " §8| " + bed);
+                score.setScore(team.getMembers().size());
+             PacketPlayOutScoreboardScore packet = new PacketPlayOutScoreboardScore(score);
+                sendPacket(packet, player);
+                i++;
+            }
+           ScoreboardScore scores = new ScoreboardScore(sb, obj, "§0§8§l§M-------------------");
+           ScoreboardScore scores2 = new ScoreboardScore(sb, obj, "§1§8§l§M-------------------");
+            scores.setScore(0);
+            scores2.setScore(i + 1);
+    PacketPlayOutScoreboardScore packet2 = new PacketPlayOutScoreboardScore(scores);
+            sendPacket(packet2, player);
+            PacketPlayOutScoreboardScore packet3 = new PacketPlayOutScoreboardScore(scores2);
+            sendPacket(packet3, player);
 
-        String map;
-        if(BedWars.getInstance().getGameHandler().getArena() != null) {
-            map = BedWars.getInstance().getGameHandler().getArena().getDisplayname().replaceAll("&", "§");
-        } else map = "-";
+        } else {
+            String map;
+            if (BedWars.getInstance().getGameHandler().getArena() != null) {
+                map = BedWars.getInstance().getGameHandler().getArena().getDisplayname().replaceAll("&", "§");
+            } else map = "-";
 
-        PacketPlayOutScoreboardObjective removePacket = new PacketPlayOutScoreboardObjective(obj, 1);
-        sendPacket(removePacket, player);
-        sendPacket(createpacket, player);
-        sendPacket(display, player);
+            PacketPlayOutScoreboardObjective removePacket = new PacketPlayOutScoreboardObjective(obj, 1);
+            sendPacket(removePacket, player);
+            sendPacket(createpacket, player);
+            sendPacket(display, player);
 
-        int i = 16;
-        for(String line : ConfigEntries.SCOREBOARD.getAsString().split("\n")){
-            line = line.replaceAll("%MAP%", map);
-            line = line.replaceAll("%KILLS%", String.valueOf(playerStats.get(PlayerStatsType.KILLS)));
-            line = line.replaceAll("%DEATHS%", String.valueOf(playerStats.get(PlayerStatsType.DEATHS)));
-            line = line.replaceAll("%RANK%", String.valueOf(playerStats.getRank()));
-            line = line.replaceAll("%KD%", String.valueOf(playerStats.getKD()));
+            int i = 16;
+            for (String line : ConfigEntries.SCOREBOARD.getAsString().split("\n")) {
+                line = line.replaceAll("%MAP%", map);
+                line = line.replaceAll("%KILLS%", String.valueOf(playerStats.get(PlayerStatsType.KILLS)));
+                line = line.replaceAll("%DEATHS%", String.valueOf(playerStats.get(PlayerStatsType.DEATHS)));
+                line = line.replaceAll("%RANK%", String.valueOf(playerStats.getRank()));
+                line = line.replaceAll("%BEDS%", String.valueOf(playerStats.get(PlayerStatsType.BEDS)));
+                line = line.replaceAll("%KD%", String.valueOf(playerStats.getKD()));
 
 
-            ScoreboardScore score = new ScoreboardScore(sb, obj, line);
-            score.setScore(i);
+                ScoreboardScore score = new ScoreboardScore(sb, obj, line);
+                score.setScore(i);
 
-            PacketPlayOutScoreboardScore packet= new PacketPlayOutScoreboardScore(score);
-            sendPacket(packet, player);
+                PacketPlayOutScoreboardScore packet = new PacketPlayOutScoreboardScore(score);
+                sendPacket(packet, player);
 
-            i--;
+                i--;
+            }
         }
     }
     public void sendPacket(Packet<?> packet, Player player) {
